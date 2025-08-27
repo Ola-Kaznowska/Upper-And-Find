e1cb2f78025569d3870be67a9aa58752f23ebee8
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2025-08-27 01:24:21 -0700

    Add files via upload

---
+++ text_find_and_upper.py
@@ -0,0 +1,32 @@
+Hello = "Python is very easy"
+
+x = Hello.find("is")
+print(x)
+
+My_opinion = "I like Python, and C#."
+y = My_opinion.find(",")
+print(y)
+
+English = "I very like English."
+Language = "I would like B1 English level."
+B1 = English.find("B1")
+print(English)
+
+
+
+print(Hello.upper())        
+print(My_opinion.upper())   
+print(English.upper())      
+print(Language.upper())     
+
+
+Shout = "learning python is fun!"
+print(Shout.upper())       
+
+Country = "I live in Poland."
+print(Country.upper())  
+
+
+My_dream = "My dream is to visit South Africa and Los Angeles."
+print(My_dream.upper())
+
